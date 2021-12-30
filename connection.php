0c606a8d6e18c08a8926809b1f7143428ec1de34
Umang-IIT <73515380+Umang-IIT@users.noreply.github.com>
2021-12-31 00:43:28 +0530

    created frontend of checklist

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+    $servername = "db4free.net:3306";
+    $username = "umangsingla617";
+    $password = "12345678";
+    $dbname = "grandreunion";
+    
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+
+?>
