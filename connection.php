7fe73d7ba493147e7b40eec41766e4b588d8da55
Umang-IIT <73515380+Umang-IIT@users.noreply.github.com>
2022-03-01 00:05:34 +0530

    db credentials added

--- connection.php
+++ connection.php
@@ -1,12 +1,12 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "aam";
-    
+$servername = "db4free.net:3306";
+$username = "umangsingla617";
+$password = "12345678";
+    $dbname = "grandreunion";
+
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
-    
+
     // Check connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
